a4b02b13da7242e468f1dd14ae61674ed598e58a
sena-ahammed <75157319+sena-ahammed@users.noreply.github.com>
2021-06-27 18:27:01 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+  <h1>Interview Room</h1>
+  <p>Students are given mock interviews from here</p>
+      </div>
+  );
+}
+export default Info;
